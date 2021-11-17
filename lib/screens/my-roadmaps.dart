93b3cbeaba1026f280fca2269bd603ac2c92756a
Paulo Kim <paulojskim@Usp.br>
2021-11-17 09:29:03 -0500

    refactored

--- lib/screens/my-roadmaps.dart
+++ lib/screens/my-roadmaps.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:rp_project/routes.dart';
 import 'package:rp_project/widgets/clickable_card.dart';
 import 'package:rp_project/widgets/header.dart';
 import 'package:rp_project/widgets/roadmap-progress.dart';
 
 class MyRoadmapsScreen extends StatelessWidget {
+  void _addNewRoadmap(context) {
+    Navigator.of(context).pushReplacementNamed(Routes.newCustomRoadmap);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +24,7 @@ class MyRoadmapsScreen extends StatelessWidget {
               progress: '10%',
               checkpoint: 'Ontem',
             ),
+            onTap: () => _addNewRoadmap(context),
           ),
           ClickableCardWidget(
             title: 'React Avançado',
@@ -26,18 +32,21 @@ class MyRoadmapsScreen extends StatelessWidget {
               progress: '25%',
               checkpoint: '27/10/2021',
             ),
+            onTap: () => _addNewRoadmap(context),
           ),
           ClickableCardWidget(
             title: 'Adicionar um novo roadmap',
             prefixWidget: Icon(Icons.add_circle_outline_outlined),
             sufixWidget: SizedBox(),
             description: 'Bora começar mais uma trilha?',
+            onTap: () => _addNewRoadmap(context),
           ),
           ClickableCardWidget(
             title: 'Criar roadmap customizado',
             prefixWidget: Icon(Icons.add_circle_outline_outlined),
             sufixWidget: SizedBox(),
             description: 'Bora começar mais uma trilha?',
+            onTap: () => _addNewRoadmap(context),
           ),
         ],
       ),
